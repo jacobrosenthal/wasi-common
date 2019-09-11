31e4d608dd9916cbe55268151b7c364b87252095
Jacob Rosenthal <jacobrosenthal@gmail.com>
2019-09-11 00:49:25 -0700

    enc_inode into u64

--- src/sys/unix/host_impl.rs
+++ src/sys/unix/host_impl.rs
@@ -237,7 +237,7 @@ pub(crate) fn dirent_from_host(host_entry: &nix::libc::dirent) -> Result<wasm32:
     if d_namlen > u32::max_value() as usize {
         return Err(host::__WASI_EIO);
     }
-    entry.d_ino = memory::enc_inode(host_entry.d_ino);
+    entry.d_ino = memory::enc_inode(host_entry.d_ino.into());
     entry.d_next = memory::enc_dircookie(host_entry.d_off as u64);
     entry.d_namlen = memory::enc_u32(d_namlen as u32);
     entry.d_type = memory::enc_filetype(host_entry.d_type);
@@ -247,7 +247,7 @@ pub(crate) fn dirent_from_host(host_entry: &nix::libc::dirent) -> Result<wasm32:
 #[cfg(not(target_os = "linux"))]
 pub(crate) fn dirent_from_host(host_entry: &nix::libc::dirent) -> Result<wasm32::__wasi_dirent_t> {
     let mut entry = unsafe { std::mem::zeroed::<wasm32::__wasi_dirent_t>() };
-    entry.d_ino = memory::enc_inode(host_entry.d_ino);
+    entry.d_ino = memory::enc_inode(host_entry.d_ino.into());
     entry.d_next = memory::enc_dircookie(host_entry.d_seekoff);
     entry.d_namlen = memory::enc_u32(u32::from(host_entry.d_namlen));
     entry.d_type = memory::enc_filetype(host_entry.d_type);
